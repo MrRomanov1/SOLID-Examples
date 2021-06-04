e17515eb2c55025854668fca5b2cfe4a0bf4e48c
MrRomanov1 <piotr.romanczak@gmail.com>
2021-06-04 19:24:34 +0200

    Added single-responsibility principle example

---
+++ src/main/java/single_responsibility_principle/correct/Account.java
@@ -0,0 +1,47 @@
+package single_responsibility_principle.correct;
+
+public class Account {
+    public String firstName;
+    public String lastName;
+    public int id;
+    public String email;
+
+    public Account(String firstName, String lastName, int id, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.email = Validation.validateEmail(email);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/single_responsibility_principle/correct/Validation.java
@@ -0,0 +1,8 @@
+package single_responsibility_principle.correct;
+
+public class Validation {
+    public static String validateEmail(String email) {
+        //validation code goes here
+        return email;
+    }
+}
